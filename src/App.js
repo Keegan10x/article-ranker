76b762e4242493248e32cccce205aad7bb392568
Keegan Pereira <keeganpereira222@gmail.com>
2021-06-23 06:00:35 +0800

    Created blank react app

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import ShowArticle from './ShowArticle'
+
+function App() {
+  return (
+    <ShowArticle />
+  )
+}
+
+export default App;
---
+++ src/ShowArticle.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ShowArticle() {
+    return (
+        <div>
+           Hello World 
+        </div>
+    )
+}
