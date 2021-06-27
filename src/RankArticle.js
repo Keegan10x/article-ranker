dac228da5e2f63e7cd15fa54af164a472847892b
Keegan Pereira <keeganpereira222@gmail.com>
2021-06-27 20:24:07 +0800

    Displaying dynamically changing slider values in rank component

--- src/RankArticle.js
+++ src/RankArticle.js
@@ -32,34 +32,32 @@ export default class RankArticle extends React.Component {
                 <h1>Rank The Articles</h1>
                 
                 <hr></hr>
-                <label for="article1">Rank Article 1</label><br></br>
+                <label for="article1">1st article rank: {this.state.article1_rank}</label><br></br>
                 <input type="range" name="article1_rank" value={this.state.article1_rank} min="0" max="5"
                 onChange={event => this.change(event)}/>
                 <hr></hr>
                 
-                <label for="article2">Rank Article 2</label><br></br>
+                <label for="article2">2nd article rank: {this.state.article2_rank}</label><br></br>
                 <input type="range" name="article2_rank" value={this.state.article2_rank} min="0" max="5"
                 onChange={event => this.change(event)}/>
                 <hr></hr>
 
-                <label for="article3">Rank Article 3</label><br></br>
+                <label for="article3">3rd article rank: {this.state.article3_rank}</label><br></br>
                 <input type="range" name="article3_rank" value={this.state.article3_rank} min="0" max="5"
                 onChange={event => this.change(event)}/>
                 <hr></hr>
 
-                <label for="article4">Rank Article 4</label><br></br>
+                <label for="article4">4th article rank: {this.state.article4_rank}</label><br></br>
                 <input type="range" name="article4_rank" value={this.state.article4_rank} min="0" max="5"
                 onChange={event => this.change(event)}/>
                 <hr></hr>
 
-                <label for="article5">Rank Article 5</label><br></br>
+                <label for="article5">5th article rank: {this.state.article5_rank}</label><br></br>
                 <input type="range" name="article5_rank" value={this.state.article5_rank} min="0" max="5"
                 onChange={event => this.change(event)}/>
                 <hr></hr>
 
-
                 <button onClick={(event) => this.onSubmit(event)}>Submit</button>
-
             </form>
             </main>
         )
